6c1eec6ea8c1cb453b57197977faf531c80a4553
Benjamin Schandera <bens@spreadshirt.net>
2014-12-04 17:33:07 +0100

    update TODOs
    - implement blog data structure and
    - html rendering
    - all mandatory tasks done :-)

--- script-1.rb
+++ script-1.rb
@@ -34,10 +34,59 @@ Blog.setCreationDate(#ofPost, creationDate)
 ==============
 =end
 
+
+
+#  BLOG DATA STRUCTURE
+class Blog
+
+  def initialize(title = "Super Secret Octo Blog")
+    setTitle(title)
+    @body = "<body>\n"
+  end
+
+  def appendPost(postTitle, body, creationDate)
+    @body << "<h1>" + postTitle + "</h1>\n"
+    @body << "<p>" + body + "</p>\n"
+    @body << "<p> <i>" + creationDate.inspect + "</i> </p>\n"
+    @body << "<hr>\n"
+  end
+  
+  def renderToHTMLFile(destinationFile)
+    htmlDocument = "<!DOCTYPE html><html>\n"
+    htmlDocument << "<!-- created " + Time.new.inspect + " -->\n"
+    htmlDocument << @title + "\n"
+    htmlDocument << @body + "</body>\n</html>\n"
+    File.open(destinationFile, "w") do |f| 
+      f << htmlDocument
+    end
+  end
+
+  def setTitle(title)
+    @title = "<head><title>" + title + "</title></head>"
+  end
+  
+  def getTitle
+    @title
+  end
+  
+  def getBody
+    @body
+  end
+  
+end
+
+
+#  PROCEDURE
 files = []
-Dir.foreach("./posts") {|file| 
+Dir.foreach("./posts") do |file| 
   if !file.eql?(".") && !file.eql?("..")
     files << File.new("./posts/" + file)
   end
-}
-files.map { |file| puts File.basename(file, ".txt"), File.read(file), file.mtime}
+end
+
+blog = Blog.new
+files.each do |file|
+  blog.appendPost(File.basename(file, ".txt"), File.read(file), file.mtime)
+end
+
+blog.renderToHTMLFile("./output-secret.html")
