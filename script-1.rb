6c2eacc4c2044e3ea06968bacd4463f5021ac39c
Benjamin Schandera <bens@spreadshirt.net>
2014-12-04 14:33:17 +0100

    add some useful examples

--- script-1.rb
+++ script-1.rb
@@ -3,6 +3,9 @@ FileUtils, File, Dir, Arrays, Hash, String
 <<, map, do .. end, [].each, 
 entries = Dir.entries "/Users/bens/"
 File.open("/Home/bens/ssh-key-2014-07-01.pub", "a") do |f|
+File.mtime("file");
+WRITE: File.open("/Home/comics.txt", "a") do |f| f << "Cat and Girl" end
+READ: print File.read("/Home/comics.txt")
 ..
 end
 =end
